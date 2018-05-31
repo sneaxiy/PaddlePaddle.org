94db916b509e25e9b5d0ae46e100a067f965d70b
Varun Arora <aroravarun@baidu.com>
2018-05-30 17:05:18 -0700

    First version of stripping is stable

--- portal/deploy/strip.py
+++ portal/deploy/strip.py
@@ -77,13 +77,13 @@ def sphinx(original_documentation_dir, generated_documentation_dir, destination_
     # if generated_documentation_dir:
     #     generated_documentation_dir = generated_documentation_dir.rstrip('/')
     #
-    if not new_path_map:
-        new_path_map = {
-            'develop': {
-                '/fluid/en/html/': '/fluid/en/',
-                '/fluid/cn/html/': '/fluid/zh/'
-            }
-        }
+    # if not new_path_map:
+    #     new_path_map = {
+    #         'develop': {
+    #             '/fluid/en/html/': '/fluid/en/',
+    #             '/fluid/cn/html/': '/fluid/zh/'
+    #         }
+    #     }
     #         '0.10.0': {
     #             '/en/html/':    '/en/',
     #             '/cn/html/': '/zh/',
@@ -96,7 +96,7 @@ def sphinx(original_documentation_dir, generated_documentation_dir, destination_
 
     # if the version is not supported, fall back to 'develop'
     # if version not in new_path_map:
-    version = 'develop'
+    # version = 'develop'
 
     # Go through each file, and if it is a .html, extract the .document object
     #   contents
@@ -104,78 +104,86 @@ def sphinx(original_documentation_dir, generated_documentation_dir, destination_
         for file in all_files:
             subpath = os.path.join(subdir, file)[len(
                 generated_documentation_dir):]
-            if version in new_path_map:
-                subpath_language_dirs = new_path_map[version].keys()
+            # if version in new_path_map:
+                # subpath_language_dirs = new_path_map[version].keys()
 
-                for subpath_language_dir in subpath_language_dirs:
+                # for subpath_language_dir in subpath_language_dirs:
                     # Check if the we should process the file or not
-                    print subpath
-                    if subpath_language_dir and subpath.startswith(subpath_language_dir):
-                        new_path = destination_documentation_dir + (
-                            new_path_map[version][subpath_language_dir]
-                            + subpath[len(subpath_language_dir):])
+                    # if subpath_language_dir and subpath.startswith(subpath_language_dir):
 
-                        if '.html' in file or '_images' in subpath or '.txt' in file or '.json' in file:
-                            if not os.path.exists(os.path.dirname(new_path)):
-                                os.makedirs(os.path.dirname(new_path))
+            if not subpath.startswith('/.') and not subpath.startswith('/_'):
+                # new_path = destination_documentation_dir + (
+                #     new_path_map[version][subpath_language_dir]
+                #     + subpath[len(subpath_language_dir):])
+                new_path = destination_documentation_dir + subpath
 
-                        if '.html' in file:
-                            # Soup the body of the HTML file.
-                            # Check if this HTML was generated from Markdown
-                            original_md_path = get_original_markdown_path(original_documentation_dir,
-                                                                          subdir, subpath_language_dir, file)
-                            if original_md_path:
-                                # If this html file was generated from Sphinx MD, we need to regenerate it using python's
-                                # MD library.  Sphinx MD library is limited and doesn't support tables
-                                markdown_file(original_md_path, version, '', new_path)
+                if '.html' in file or '_images' in subpath or '.txt' in file or '.json' in file:
+                    if not os.path.exists(os.path.dirname(new_path)):
+                        os.makedirs(os.path.dirname(new_path))
 
-                                # Since we are ignoring SPHINX's generated HTML for MD files (and generating HTML using
-                                # python's MD library), we must fix any image links that starts with 'src/'.
-                                image_subpath = None
-                                relative_path = _find_relative_path(subdir, subpath_language_dir)
-                                if relative_path:
-                                    # figure out the relative path of the file to the root, and append a ../ on each parent
-                                    parent_paths = relative_path.split('/')
-                                    image_subpath = ''
-                                    for i in range(len(parent_paths)):
-                                        image_subpath = image_subpath + '../'
-                                    image_subpath += '_images'  # hardcode the sphinx '_images' dir
+                if '.html' in file:
+                    # Soup the body of the HTML file.
+                    # Check if this HTML was generated from Markdown
+                    # original_md_path = get_original_markdown_path(original_documentation_dir,
+                    #                                               subdir, subpath_language_dir, file)
 
-                                with open(new_path) as original_html_file:
-                                    soup = BeautifulSoup(original_html_file, 'lxml')
+                    original_md_path = get_original_markdown_path(
+                        original_documentation_dir, subpath[1:])
 
-                                    image_links = soup.find_all('img', src=re.compile(r'^(?!http).*'))
+                    if original_md_path:
+                        # If this html file was generated from Sphinx MD, we need to regenerate it using python's
+                        # MD library.  Sphinx MD library is limited and doesn't support tables
+                        markdown_file(original_md_path, version, '', new_path)
 
-                                    if len(image_links) > 0:
-                                        for image_link in image_links:
-                                            image_file_name = os.path.basename(image_link['src'])
+                        # Since we are ignoring SPHINX's generated HTML for MD files (and generating HTML using
+                        # python's MD library), we must fix any image links that starts with 'src/'.
+                        image_subpath = None
 
-                                            if image_subpath:
-                                                image_link['src'] = '%s/%s' % (image_subpath, image_file_name)
-                                            else:
-                                                image_link['src'] = '_images/%s' % (image_file_name)
+                        #relative_path = _find_relative_path(subdir, subpath_language_dir)
+                        #if relative_path:
+                        # figure out the relative path of the file to the root, and append a ../ on each parent
+                        parent_paths = subpath.split('/')
+                        image_subpath = ''
+                        for i in range(len(parent_paths)):
+                            image_subpath = image_subpath + '../'
+                        image_subpath += '_images'  # hardcode the sphinx '_images' dir
 
-                                        with open(new_path, 'w') as new_html_partial:
-                                            new_html_partial.write(soup.encode("utf-8"))
-                            else:
-                                with open(os.path.join(subdir, file)) as original_html_file:
-                                    soup = BeautifulSoup(original_html_file, 'lxml')
+                        with open(new_path) as original_html_file:
+                            soup = BeautifulSoup(original_html_file, 'lxml')
 
-                                document = None
-                                # Find the .document element.
-                                if version == '0.9.0':
-                                    document = soup.select('div.body')[0]
-                                else:
-                                    document = soup.select('div.document')[0]
+                            image_links = soup.find_all('img', src=re.compile(r'^(?!http).*'))
+
+                            if len(image_links) > 0:
+                                for image_link in image_links:
+                                    image_file_name = os.path.basename(image_link['src'])
+
+                                    if image_subpath:
+                                        image_link['src'] = '%s/%s' % (image_subpath, image_file_name)
+                                    else:
+                                        image_link['src'] = '_images/%s' % (image_file_name)
+
                                 with open(new_path, 'w') as new_html_partial:
-                                    new_html_partial.write(document.encode("utf-8"))
-                        elif '_images' in subpath or '.txt' in file or '.json' in file:
-                            # Copy to images directory.
-                            copyfile(os.path.join(subdir, file), new_path)
-                        elif 'searchindex.js' in subpath:
-                            copyfile(os.path.join(subdir, file), new_path)
+                                    new_html_partial.write(soup.encode("utf-8"))
+                    else:
+                        with open(os.path.join(subdir, file)) as original_html_file:
+                            soup = BeautifulSoup(original_html_file, 'lxml')
+
+                        document = None
+                        # Find the .document element.
+                        if version == '0.9.0':
+                            document = soup.select('div.body')[0]
+                        else:
+                            document = soup.select('div.document')[0]
+                        with open(new_path, 'w') as new_html_partial:
+                            new_html_partial.write(document.encode("utf-8"))
+                elif '_images' in subpath or '.txt' in file or '.json' in file:
+                    # Copy to images directory.
+                    copyfile(os.path.join(subdir, file), new_path)
+                elif 'searchindex.js' in subpath:
+                    copyfile(os.path.join(subdir, file), new_path)
 
     print 'strip.sphinx done'
+
 
 def remove_old_dir(version, output_dir_name):
     destination_documentation_dir = _get_destination_documentation_dir(version, output_dir_name)
@@ -191,7 +199,7 @@ def _find_relative_path(path, subpath_language_dir):
     return relative_path
 
 
-def get_original_markdown_path(original_documentation_dir, path, subpath_language_dir, file):
+def get_original_markdown_path(original_documentation_dir, file):
     """
     Finds the path of the original MD file that generated the html file located at "path"
     :param original_documentation_dir:
@@ -200,9 +208,10 @@ def get_original_markdown_path(original_documentation_dir, path, subpath_languag
     :param file:
     :return:
     """
-    relative_path = _find_relative_path(path, subpath_language_dir)
+    # relative_path = _find_relative_path(path, subpath_language_dir)
     filename, _ = os.path.splitext(file)
-    original_file_path = '%s/doc/%s/%s.md' % (original_documentation_dir, relative_path, filename)
+    # original_file_path = '%s/doc/%s/%s.md' % (original_documentation_dir, relative_path, filename)
+    original_file_path = '%s/%s.md' % (original_documentation_dir, filename)
 
     if os.path.isfile(original_file_path):
         return original_file_path
